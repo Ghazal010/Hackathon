75649f50f05240bd8f57314acd291e9ff9fa26a1
Ghazal Shaikh <159722197+Ghazal010@users.noreply.github.com>
2024-09-08 16:03:44 +0500

    Update hackathon.ts

--- hackathon.ts
+++ hackathon.ts
@@ -1,18 +1,17 @@
-// Function to scroll to the top of the page
+
 function scrollToTop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
-// Function to set up event listeners
+
 function setupEventListeners(): void {
     const backToTopLink = document.getElementById('backToTopLink') as HTMLAnchorElement;
     if (backToTopLink) {
         backToTopLink.addEventListener('click', (event: MouseEvent) => {
-            event.preventDefault(); // Prevent default anchor click behavior
-            scrollToTop(); // Call function to scroll to the top
+            event.preventDefault(); 
+            scrollToTop(); 
         });
     }
 }
 
-// Initialize the script
 document.addEventListener('DOMContentLoaded', setupEventListeners);
